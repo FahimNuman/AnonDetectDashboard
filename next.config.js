f6d9c036bf7e5b58f6d965f2e6792ec969daf4f9
FahimNuman87 <fahiomnuman87@gmail.com>
2024-09-01 10:53:39 +0600

    anon

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
       {
         protocol: "https",
-        hostname: "investment-server-wwat.vercel.app",
+        hostname: "anon-hctxlnvkx-fahimnumans-projects.vercel.app",
         pathname: "**",
       },
       {
